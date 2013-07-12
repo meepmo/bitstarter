5a53356fd2e3fff2574219ed4cb7da952941284f
Billy Gozum <billy.acct@gmail.com>
2013-07-12 15:23:24 +0000

    Fixed to render on heroku

--- web.js
+++ web.js
@@ -1,8 +1,17 @@
-#!/usr/bin/env node
+var express = require('express');
 var fs = require('fs');
 
-var infile = "index.html";
-var inString = fs.readFileSync(infile);
+var inputFile = "index.html";
+var buf = new Buffer();
+var out = fs.readFileSync(inputFile);
 
-var buf = new Buffer(inString);
-console.log(buf.toString());
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+	response.send(out.toString());
+});
+
+console.log(outtoString('utf-8'));
+
+var port = process.env.PORT || 5000;
+app.listen(port);
