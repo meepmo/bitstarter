ffc216722b316882311807b261860c65c4d6dab6
Billy Gozum <billy.acct@gmail.com>
2013-07-12 15:43:42 +0000

    Fixed buffer output

--- web.js
+++ web.js
@@ -2,16 +2,16 @@ var express = require('express');
 var fs = require('fs');
 
 var fileIn = "index.html";
-var buf = new Buffer();
+var buf = new Buffer(50);
+buf = fs.readFileSync(fileIn);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = fs.readFileSync(fileIn);
-  response.send(buf.toString());
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port + "\n" + buf.toString());
+  console.log("Listening on " + port + "\n" + buf.toString('utf-8'));
 });
