4ba9e60715da12e3ef98ba79155cdc4b07c57bdb
Billy Gozum <billy.acct@gmail.com>
2013-07-12 15:31:52 +0000

    reverted

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
 var express = require('express');
-var fs = require('fs');
-
-var inputFile = "index.html";
-var buf = new Buffer();
-var out = fs.readFileSync(inputFile);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	response.send(out.toString());
+  response.send('Hello World!');
 });
 
-console.log(outtoString('utf-8'));
-
 var port = process.env.PORT || 5000;
-app.listen(port);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
