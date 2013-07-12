96a28396b9d0fd354a910ebb37b812a4af28504a
Billy Gozum <billy.acct@gmail.com>
2013-07-12 14:51:41 +0000

    Modified to pull from index.html

--- web.js
+++ web.js
@@ -1,14 +1,7 @@
-var express = require('express');
+#!/usr/bin/env node
+var fs = require('fs');
+var infile = "index.html";
+var inString = fs.readFileSync(infile);
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  var out = fs.readFileSync("index.html").toString;
-
-  response.send(out);
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var buf = new Buffer(inString);
+console.log(buf.toString());
