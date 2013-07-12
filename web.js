3e5453236f0d3de042047c51ec09a9d2756ade53
Billy Gozum <billy.acct@gmail.com>
2013-07-12 15:39:08 +0000

    Fixed to read index.html properly

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+
+var fileIn = "index.html";
+var buf = new Buffer();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  buf = fs.readFileSync(fileIn);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on " + port + "\n" + buf.toString());
 });
