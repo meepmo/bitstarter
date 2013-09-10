db43738854df31cf98c12a6642fd70c52f330684
Billy <billy.acct@gmail.com>
2013-09-10 22:02:21 +0000

    Updated copy, AddThis, PayPal, and social links

--- js/custom.js
+++ js/custom.js
@@ -161,6 +161,19 @@ $(".nav li a[href^='#']").click(function(event) {
         scrollTop: target_top
     }, 1500, 'easeInOutExpo');
 });
+
+$("a[href^='#sponsorBenefits']").click(function(event) {
+    event.preventDefault();
+    var full_url = this.href;
+    var parts = full_url.split("#");
+    var trgt = parts[1];
+
+    var target_offset = $("#" + trgt).offset();
+    var target_top = target_offset.top - 0;
+    $('html, body').animate({
+        scrollTop: target_top
+    }, 1500, 'easeInOutExpo');
+});
 //
 //	
 
